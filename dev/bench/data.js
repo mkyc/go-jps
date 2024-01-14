6693a979c6cf726eeefcdaa2218bfcb98f7fdf58
github-action-benchmark <github@users.noreply.github.com>
2024-01-14 13:48:44 +0000

    add Benchmark (go) benchmark result for 38d8d0677d5673c7f39a04c7e35b2bf44a3e2a76

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1705239827450,
+  "lastUpdate": 1705240124141,
   "repoUrl": "https://github.com/mkyc/go-jps",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 1584497,
             "unit": "ns/op",
             "extra": "710 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mateusz.kyc@gmail.com",
+            "name": "Mateusz Kyc",
+            "username": "mkyc"
+          },
+          "committer": {
+            "email": "mateusz.kyc@gmail.com",
+            "name": "Mateusz Kyc",
+            "username": "mkyc"
+          },
+          "distinct": true,
+          "id": "38d8d0677d5673c7f39a04c7e35b2bf44a3e2a76",
+          "message": "add GH action to publish benchmark results",
+          "timestamp": "2024-01-14T14:46:24+01:00",
+          "tree_id": "449e4bcf9c63fc08cd47eb93f27bc1f2a65a5fa7",
+          "url": "https://github.com/mkyc/go-jps/commit/38d8d0677d5673c7f39a04c7e35b2bf44a3e2a76"
+        },
+        "date": 1705240123293,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFind",
+            "value": 1597244,
+            "unit": "ns/op",
+            "extra": "732 times\n4 procs"
           }
         ]
       }
