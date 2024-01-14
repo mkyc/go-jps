10d2edfcdec909a2a68ded1e896b9b024dea86c1
github-action-benchmark <github@users.noreply.github.com>
2024-01-14 22:12:02 +0000

    add Benchmark (go) benchmark result for 1d4d87fde8e889321156e31caa2006c235c3ec93

---
+++ dev/bench/1.21.4/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1705270322368,
+  "repoUrl": "https://github.com/mkyc/go-jps",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mateusz.kyc@gmail.com",
+            "name": "Mateusz Kyc",
+            "username": "mkyc"
+          },
+          "committer": {
+            "email": "mateusz.kyc@gmail.com",
+            "name": "Mateusz Kyc",
+            "username": "mkyc"
+          },
+          "distinct": true,
+          "id": "1d4d87fde8e889321156e31caa2006c235c3ec93",
+          "message": "hmmmm 3",
+          "timestamp": "2024-01-14T23:11:33+01:00",
+          "tree_id": "4ee5960c07edb625590f1dd8f8e3238fa0116592",
+          "url": "https://github.com/mkyc/go-jps/commit/1d4d87fde8e889321156e31caa2006c235c3ec93"
+        },
+        "date": 1705270321859,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFind",
+            "value": 1570205,
+            "unit": "ns/op",
+            "extra": "760 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
